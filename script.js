ac00491e860e9d3eec2ce1bb26e1d605bf1ec886
Danny Sanchez <dannysanchez559@gmail.com>
2020-10-20 22:48:02 -0700

    Nav and content scroll UI update

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
+// for sticky nav
+$(document).scroll(function() {
+    var y = $(document).scrollTop(), //get page y value
+        header = $("#header-box"); // your div id
+    if(y >= 100)  {
+        header.css({
+          "background-color" : "rgba(255, 255, 255, 0.98)",
+          "transition" : "background-color 0.5s ease 0.3s"
+          });
+    } else {
+        header.css({
+          "background-color" : "rgba(255, 255, 255, 0)"
+        });
+    }
+});
+
+// nav smooth scrolling
 function scrollSetup(element) {
   const tagName = element.innerHTML;
 
   switch (tagName) {
     case 'About':
-    document.querySelector('.about-me').scrollIntoView({ behavior: 'smooth' });
+    window.scroll({ top: 0, left: 0, behavior: 'smooth' });
+    // document.querySelector('.header').scrollIntoView({ behavior: 'smooth' });
     break;
 
     case 'Projects':
@@ -23,6 +41,19 @@ function scrollSetup(element) {
   }
 }
 
-function scrollToTopSetup() {
+function contentScroll(element) {
+  var button = element.id;
+  var content = document.querySelector('.content-list');
+
+  // scroll left
+  if (button == "left-button") {
+    // console.log(button.id)
+    content.scrollBy({ left: -360, top: 0, behavior: 'smooth' });
+  }
+  // scroll right
+  else {
+    content.scrollBy({ left: 360, top: 0, behavior: 'smooth' });
+    // console.log(button.id)
+  }
 
 }
